d3f80c0abe3f6da3ab0b6e9da775540c56d24dc1
TimHoremans <tim.horemans97@gmail.com>
2023-07-24 22:19:54 +0200

    Add behave setup and start for linear patrolling

---
+++ game-darksouls/Entity/Behaviour/LinearPatrol.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game_darksouls.Entity.Behaviour
+{
+    public class LinearPatrol : IBehave
+    {
+        private readonly AnimatedObject animatedObject;
+
+        private Vector2 positionA;
+        private Vector2 positionB;
+        private Vector2 currentTarget;
+
+        public LinearPatrol(Vector2 positionA, Vector2 positionB, AnimatedObject animatedObject)
+        {
+            this.positionA = positionA;
+            this.positionB = positionB;
+
+            currentTarget = positionA;
+
+            this.animatedObject = animatedObject;
+        }
+
+        public void Behave()
+        {
+            
+        }
+    }
+}
