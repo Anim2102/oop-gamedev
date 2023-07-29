c369e4535afd07e7452b726bc51dacd5eef9820d
TimHoremans <tim.horemans97@gmail.com>
2023-07-29 16:22:24 +0200

    Setup camera

---
+++ game-darksouls/Utilities/Camera.cs
@@ -0,0 +1,26 @@
+﻿using game_darksouls.Entity;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace game_darksouls.Utilities
+{
+    internal class Camera
+    {
+        public Matrix Matrix { get; set; }
+        public Vector2 CameraPosition { get; set; }
+
+        private Viewport viewport;
+        private Player player;
+
+        public Camera(int x,int y, int width, int height, Player player)
+        {
+            viewport = new Viewport(x,y,width,height);
+            this.player = player;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
